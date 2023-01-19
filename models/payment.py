4988a057d497cc0b05bdc40ec67e1349d1baef6b
nikitashinde1 <nikita.shinde@sky.uk>
2023-01-19 12:18:23 +0000

    Update import statements in all files under module with respect to the refactoring

--- models/payment.py
+++ models/payment.py
@@ -1,5 +1,5 @@
 class Payment:
-    def process_a_payment(self, order, payment_info):
+    def process_payment(self, order, payment_info):
         card_number = payment_info['card_number']
         expiry_date = payment_info['expiry_date']
         cvv = payment_info['cvv']
@@ -10,7 +10,7 @@ class Payment:
         # For this example, I will just simulate a successful payment
         success = True
 
-        if success:
+        if payment_info.get('payment_status')== 'success':
             print("Payment successful!")
             order.status = "PAID"
             return True

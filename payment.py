457f4cdfa97301a877270dd85369f0ae3fd71eac
kara <karahan.ozturk2@sky.uk>
2023-04-20 11:56:50 +0300

    Improve logging

--- payment.py
+++ payment.py
@@ -1,4 +1,7 @@
 class Payment:
+    def last_four_chars(self, string):
+        return string[-4:]
+
     def process_a_payment(self, order, payment_info):
         card_number = payment_info['card_number']
         expiry_date = payment_info['expiry_date']
@@ -12,9 +15,11 @@ class Payment:
 
         if success:
             print("Payment successful!")
+            print(f"Card details: ${self.last_four_chars(card_number)}")
             order.status = "PAID"
             return True
         else:
             print("Payment failed!")
+            print(f"Card details: ${self.last_four_chars(card_number)}")
             order.status = "FAILED"
             return False
